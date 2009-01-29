0a555655cfb4021e974d79e4dbf1a282cf173a98
mcpasquier <mcpasquier@47364e80-e800-11dd-b882-2156224b14b4>
2009-01-29 17:18:04 +0000

    actual frame file to test compatibility of iframes

---
+++ trunk/sandbox/homeframe.php
@@ -0,0 +1,41 @@
+ <html>
+ <head>
+ <STYLE type="text/css">
+  SPAN { padding-left:3px; padding-right:3px }
+  DIV.header{ margin:0; padding-bottom: 1px; color: white; background-color: #000000; border:none; font-weight:bold}
+  BODY.main{ width: 744px; margin:0 auto; padding:0; background-color:#003366; color: #000000; border:outset}
+  DIV.menu{ text-align: center; border-top:1px solid white; border-bottom:1px solid white; background-color:#000000; color:white; font-weight: bold}
+  DIV.menu A:link { text-decoration: none; color:#FFFFFF; font-weight: bold }
+  DIV.menu A:visited { text-decoration: none; color:#999999 }
+  DIV.menu A:active { text-decoration: none; color:#666666 }
+  DIV.menu A:hover { text-decoration: none; color:#FF0000 }
+ </STYLE>
+ </head>
+ 
+ <body>
+
+<div align="center" class="header">
+<c>
+
+<a href = "http://disaster.stjoe-redcross.org/sandbox/home.php">
+<img src="masthead.jpg" style="width:740px; height:100px" border="0"></a>
+  			<p style="padding-bottom:1px; margin:0">
+				American Red Cross, St. Joseph County Chapter
+			</p>
+			<p style="font-weight:normal; padding:0; margin: 0">
+				<span>3220 East Jefferson Boulevard</span>
+				<span>&nbsp;</span>
+				<span>South Bend</span>
+				<span>Indiana</span>
+				<span>46615</span>
+				<span>Phone (574) 234-0191</span>
+
+			</p>
+</c>
+</div>
+<div class="menu">
+<a href = "http://disaster.stjoe-redcross.org/sandbox/home.php"> HOME</a> | 
+<a href = "http://disaster.stjoe-redcross.org/sandbox/search.php"> SEARCH </a>
+</div>
+</body>
+</html>
