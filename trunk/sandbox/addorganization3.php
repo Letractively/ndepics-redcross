d29869c44ccc88578079a8a46de23b22343c3cf5
bwinth <bwinth@47364e80-e800-11dd-b882-2156224b14b4>
2009-02-12 20:07:33 +0000

    Organization Add goes to Organization info page

--- trunk/sandbox/addorganization3.php
+++ trunk/sandbox/addorganization3.php
@@ -210,15 +210,12 @@ print "</table><br>";
 	$org_query = "DELETE FROM organization WHERE organization_name = \"".$organization_name."\"";
 	mysql_query($org_query);
 	exit (-1);
+	//$redirect_url = "./home.php";
 }
 else {
-	
-	
-	$message .= "Successful Add...redirecting<br>";
-//	$_SESSION['valid'] = "valid";
-//	$_SESSION['user_id'] = $row['user_id'];
-//	$_SESSION['access_level_id'] = $row['access_level_id'];
-	$redirect_url = "./organizationinfo.php";
+	$redirect_url = "./organizationinfo.php?id=".$row['organization_id']."\"";
+//	$message .= "Successful Add...redirecting<br>";
+
 //	//Display what values are being added to the database
 //print "<h2>Successfully Added Values: </h2>";
 //print "<table>";
@@ -288,7 +285,52 @@ $query = "INSERT INTO resource_listing (resource_id, organization_id)
 $result = mysql_query($query) or die ("Error adding resource_listing");
 
 
+	
+//<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+//<html>
+//<head>
+//<title>Disaster Database - Adding Organization</title>
+//</head>
+//
+//
+//<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+//<? print "<meta http-equiv=\"Refresh\" content=\"0.01; url=".$redirect_url."\">"; ?>
+//<meta name="resource-type" content="document">
+//<meta name="description" content="disaster.stjoe-redcross.org">
+//<meta name="keywords" content="">
+//<meta name="copyright" content="stjoe-redcross.org 2008.  All rights reserved.">
+//<link rel="shortcut icon" href="http://www.stjoe-redcross.org/favicon.ico">
+// <STYLE type="text/css">
+//  SPAN { padding-left:3px; padding-right:3px }
+//  DIV.header{ margin:0; padding-bottom: 1px; color: white; background-color: #000000; border:none; font-weight:bold}
+//  BODY.main{ width: 744px; margin:0 auto; padding:0; background-color:#003366; color: #000000; border:outset}
+// </STYLE>
+//
+//
+//<body class="main" onLoad="setTimeout('redirect()', 100)">
+//<div style="border:2px solid white; background-color:#FFFFFF">
+//
+//<iframe src ="homeframe.php" width="745px" height="175px" scrolling= "no" FRAMEBORDER="0">
+//  <h2 align="center">St. Joseph's County American Red Cross</h2>
+//  <p align="center">Your browser does not support iframes.</p>
+//  <div class="menu">
+//  <a href = "./home.php" target= "_parent"> HOME</a> | 
+//  <a href = "./search.php" target= "_parent"> SEARCH </a> |
+//  <a href = "./logout.php" target= "_parent"> LOGOUT </a>
+//  </div>
+//</iframe>
+//
+//
+//
+//<div align="center">
+//<h3> Adding organization...please wait one moment</h3>
+//
+//	
+//</div>
+
 //print "<br> Got to the end of the script <br>";
+
+
 
 print "<div align='center'>";
 print "<form action=\"./home.php\" >\n";
@@ -298,3 +340,28 @@ print "</div>";
 
 include ("config/closedb.php");
 ?>
+
+<? print "<meta http-equiv=\"Refresh\" content=\"0.01; url=".$redirect_url."\">"; ?>
+<meta name="resource-type" content="document">
+<meta name="description" content="disaster.stjoe-redcross.org">
+<meta name="keywords" content="">
+<meta name="copyright" content="stjoe-redcross.org 2008.  All rights reserved.">
+<link rel="shortcut icon" href="http://www.stjoe-redcross.org/favicon.ico">
+ <STYLE type="text/css">
+  SPAN { padding-left:3px; padding-right:3px }
+  DIV.header{ margin:0; padding-bottom: 1px; color: white; background-color: #000000; border:none; font-weight:bold}
+  BODY.main{ width: 744px; margin:0 auto; padding:0; background-color:#003366; color: #000000; border:outset}
+ </STYLE>
+
+
+<body class="main" onLoad="setTimeout('redirect()', 10000)">
+<div style="border:2px solid white; background-color:#FFFFFF">
+
+
+<h3> Logging in...you will be redirected shortly.</h3>
+
+	
+</div>
+</body>
+
+</html>
