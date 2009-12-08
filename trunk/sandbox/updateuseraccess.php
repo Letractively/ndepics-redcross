b3d83b76cfb46483cf1bf8a8a6015b00f1010f38
robwettach <robwettach@47364e80-e800-11dd-b882-2156224b14b4>
2009-12-08 17:28:45 +0000

    Fixed Issue 23 by checking for a "NULL" input and asking them to "Try Again".

--- trunk/sandbox/updateuseraccess.php
+++ trunk/sandbox/updateuseraccess.php
@@ -77,57 +77,65 @@ include("./config/functions.php");
 //
 
 print "User id: ".$_POST['user_id']."<br>";
-
-$query = "	SELECT	u.username, u.email, al.*
-			FROM	users u, access_level al
-			WHERE	u.user_id = ".$_POST['user_id']."
-			AND		u.access_level_id = al.access_level_id";
+if( $_POST['user_id'] == "NULL" ){
+	print "<center><h3>You must select a user to modify.<br />";
+	print "<form action=\"modifyuser.php\" method=\"post\">";
+	print "<input type=\"submit\" value=\"Try Again\" />";
+	print "</form></h3></center>";
+}
+else{
+	$query = "	SELECT	u.username, u.email, al.*
+				FROM	users u, access_level al
+				WHERE	u.user_id = ".$_POST['user_id']."
+				AND		u.access_level_id = al.access_level_id";
+				
+	$result = mysql_query($query) or die ("Error: Could not access the selected user's information");
+	
+	$row = mysql_fetch_assoc($result);
+	
+	print "<center><h2>".$row['username']."'s Access Rights</h2>";
+	print " <h3>An administrative level grants all user abilities listed below.</h3>";
+	
+	print "<form action=\"updateuseraccess2.php\" method=\"post\">";
+	
+	print "<input type=\"hidden\" name=\"user_id\" value=".$_POST['user_id'].">";
+	
+	print "<table align=\"center\">";
+	
+	print "	<tr>";
+	print "	<td><b>Admin</b></td>";
+	print " <td><input type=\"checkbox\" name=\"admin\" value=\"1\" ".($row['Admin']?'checked="checked"':'')."></td>";
+	print " </tr>";		
+	
+	print "	<tr>";
+	print "	<td><b>Search</b></td>";
+	print " <td><input type=\"checkbox\" name=\"search\" value=\"1\" ".($row['Search']?'checked="checked"':'')."></td>";
+	print " </tr>";
+	
+	print "	<tr>";
+	print "	<td><b>Insert</b></td>";
+	print " <td><input type=\"checkbox\" name=\"insert\" value=\"1\" ".($row['Insert']?'checked="checked"':'')."></td>";
+	print " </tr>";
 			
-$result = mysql_query($query) or die ("Error: Could not access the selected user's information");
+	print "	<tr>";
+	print "	<td><b>Delete</b></td>";
+	print " <td><input type=\"checkbox\" name=\"delete\" value=\"1\" ".($row['Delete']?'checked="checked"':'')."></td>";
+	print " </tr>";
+			
+	print "	<tr>";
+	print "	<td><b>Update</b></td>";
+	print " <td><input type=\"checkbox\" name=\"update\" value=\"1\" ".($row['Update']?'checked="checked"':'')."></td>";
+	print " </tr>";		
+	
+	print "</table>";
+	
+	print "<br>";
+	
+	print "<input type=\"submit\" value=\"Update User\">";
+	
+	print "</form>";
 
-$row = mysql_fetch_assoc($result);
-
-print "<center><h2>".$row['username']."'s Access Rights</h2>";
-print " <h3>An administrative level grants all user abilities listed below.</h3>";
-
-print "<form action=\"updateuseraccess2.php\" method=\"post\">";
-
-print "<input type=\"hidden\" name=\"user_id\" value=".$_POST['user_id'].">";
-
-print "<table align=\"center\">";
-
-print "	<tr>";
-print "	<td><b>Admin</b></td>";
-print " <td><input type=\"checkbox\" name=\"admin\" value=\"1\" ".($row['Admin']?'checked="checked"':'')."></td>";
-print " </tr>";		
-
-print "	<tr>";
-print "	<td><b>Search</b></td>";
-print " <td><input type=\"checkbox\" name=\"search\" value=\"1\" ".($row['Search']?'checked="checked"':'')."></td>";
-print " </tr>";
-
-print "	<tr>";
-print "	<td><b>Insert</b></td>";
-print " <td><input type=\"checkbox\" name=\"insert\" value=\"1\" ".($row['Insert']?'checked="checked"':'')."></td>";
-print " </tr>";
-		
-print "	<tr>";
-print "	<td><b>Delete</b></td>";
-print " <td><input type=\"checkbox\" name=\"delete\" value=\"1\" ".($row['Delete']?'checked="checked"':'')."></td>";
-print " </tr>";
-		
-print "	<tr>";
-print "	<td><b>Update</b></td>";
-print " <td><input type=\"checkbox\" name=\"update\" value=\"1\" ".($row['Update']?'checked="checked"':'')."></td>";
-print " </tr>";		
-
-print "</table>";
-
-print "<br>";
-
-print "<input type=\"submit\" value=\"Update User\">";
-
-print "</form>";
+}
 
 include ("config/closedb.php");
 ?>
