871d4a80ad5b334eaf2a7bead31e0f3dadf39adf
mattmoon9 <mattmoon9@47364e80-e800-11dd-b882-2156224b14b4>
2010-05-28 14:11:32 +0000

    redirect for logout page in search directory.

--- trunk/sandbox/search/logout.php
+++ trunk/sandbox/search/logout.php
@@ -2,6 +2,6 @@
 session_start();
 echo "<title>St. Joseph Red Cross - Redirect</title>";
 $redirect_url = "../logout.php";
-print "<h3>Redirecting to Site Map...</h3>";
+print "<h3>Logging you out...</h3>";
 print "<meta http-equiv=\"Refresh\" content=\"0.1; url=".$redirect_url."\">";
 ?>
