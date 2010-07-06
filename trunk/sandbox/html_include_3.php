306e2e58b75bb51fb3ed911135be0ecb6e4767cd
mattmoon9 <mattmoon9@47364e80-e800-11dd-b882-2156224b14b4>
2010-07-06 18:51:45 +0000

    Attempt to fix Google Analytics tracking.

--- trunk/sandbox/html_include_3.php
+++ trunk/sandbox/html_include_3.php
@@ -1,16 +1,7 @@
 </td>
 </tr>
 </table>
-<!--Google Analytics Code and Closing Tags-->
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-1322410-18");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+
 </div>
 </body>
 </html>
